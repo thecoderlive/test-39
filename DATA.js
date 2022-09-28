1f1c792fda1f404dd298de4be371fae2257e1036
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-29 02:13:34 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "a3916e1d-e6ec-40b1-915b-8146eea1c7b7",
+    "home_feed": [
+        {
+            "id": "dc6742d3-88c4-4a08-85cc-21d881ad374d",
+            "artist_name": "remain",
+            "home_carousel": [
+                {
+                    "id": "71a6b347-af46-4978-bfd2-f9628cbc3327",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/73.png"
+                },
+                {
+                    "id": "4b48eb2d-072a-4eb7-9490-445260164695",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/355.png"
+                },
+                {
+                    "id": "50faee88-412d-4a79-817b-28b8735f30cb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/298.png"
+                },
+                {
+                    "id": "70a7086f-b6bb-4e38-9198-db71d3bf855a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/190.png"
+                },
+                {
+                    "id": "4e162e92-3f99-4486-9b89-9453309fb5df",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/143.png"
+                }
+            ],
+            "art_caption": "factor",
+            "art_description": "Rate short economy fear deal what own plan."
+        },
+        {
+            "id": "3917f256-1828-4e7a-bf9d-e4951c67c609",
+            "artist_name": "we",
+            "home_carousel": [
+                {
+                    "id": "5cffea77-ed46-4c85-acbf-b1c8cc6baae9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/233.png"
+                },
+                {
+                    "id": "af20797b-7253-40c6-af77-4ab5858bfc9a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/92.png"
+                },
+                {
+                    "id": "959ee602-7c64-4a24-bfa6-1bb769486fa5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/180.png"
+                },
+                {
+                    "id": "4f709b3d-c9c2-481a-9a13-1132d80f3bb4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/119.png"
+                },
+                {
+                    "id": "d67d4fea-b41d-4347-afcd-9c6048ea93f8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/174.png"
+                }
+            ],
+            "art_caption": "hair",
+            "art_description": "Third us attorney follow trade research soldier. Describe nothing make of."
+        },
+        {
+            "id": "f9d7a7dd-5b6f-4fab-a0a8-d6f5c7f72091",
+            "artist_name": "understand",
+            "home_carousel": [
+                {
+                    "id": "7c3dd5b2-2081-4bfb-af4d-2f2c1c8d95f2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/285.png"
+                },
+                {
+                    "id": "32d135ba-ccca-4f18-8741-da9fa8dbfb1a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/176.png"
+                },
+                {
+                    "id": "b244a7bf-302f-4a2e-852d-d17c9374c9e9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/187.png"
+                },
+                {
+                    "id": "cc094690-78ed-4b8e-8044-b29cbce71684",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/192.png"
+                },
+                {
+                    "id": "041b322f-6f5a-47e1-a433-f6813040b051",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/71.png"
+                }
+            ],
+            "art_caption": "economy",
+            "art_description": "Loss some need tough enter baby than. Explain doctor thing degree wind nice design."
+        },
+        {
+            "id": "1aacb2b3-8a61-4850-b222-1df3969e0678",
+            "artist_name": "national",
+            "home_carousel": [
+                {
+                    "id": "22be0772-2af7-4ae5-b475-9491311b6cb1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/235.png"
+                },
+                {
+                    "id": "73a26665-06a3-410d-8a0b-e88a243d2e39",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/39.png"
+                },
+                {
+                    "id": "f3febbd7-1401-4606-975d-84645a5336db",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/140.png"
+                },
+                {
+                    "id": "1f8a2af7-d38e-445c-ab32-ca69a3883f02",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/201.png"
+                },
+                {
+                    "id": "13ae7340-f9d1-43ef-933b-c7e9200161e3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/178.png"
+                }
+            ],
+            "art_caption": "answer",
+            "art_description": "Modern color way leg speech area. Still program list sign talk page owner close."
+        },
+        {
+            "id": "b613857c-0291-4233-adfc-7d26c83006a4",
+            "artist_name": "already",
+            "home_carousel": [
+                {
+                    "id": "b5eef63f-8cf6-424f-b5f0-f5bb3f7ed7e5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/41.png"
+                },
+                {
+                    "id": "c2f9920d-72e8-40f3-aeff-f13130b7f967",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/182.png"
+                },
+                {
+                    "id": "12589ce9-5405-4106-8582-aac440004ed2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/235.png"
+                },
+                {
+                    "id": "00ce5ea1-0e58-4b6f-9639-8a8e0c66f649",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/351.png"
+                },
+                {
+                    "id": "9cbdb27d-23ef-4a8d-b1b8-5ceb6cb9b0fc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/344.png"
+                }
+            ],
+            "art_caption": "physical",
+            "art_description": "Factor will minute natural music specific culture. Lead security animal area region direction. Well unit chair in."
+        }
+    ]
+}
